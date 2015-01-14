163cf2b82b0037f3ed7f900e45e473207b381cd2
twoface <simonxiao@qq.com>
2015-01-14 12:01:58 +0800

    v0.1.1-SNAPSHOT

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -10,7 +10,7 @@ import bintray.Plugin._
 import bintray.Keys._
 
 object BuildSettings {
-  val VERSION = "0.1.1a1"
+  val VERSION = "0.1.1-SNAPSHOT"
 
   lazy val repo =  if (VERSION.endsWith("SNAPSHOT")) "snapshot" else "release"
 
